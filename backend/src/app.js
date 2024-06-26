ac15962725cef3a1c77627694925f8daa37d2fa0
Nishan Bista <nishanbista525@gmail.com>
2024-06-26 23:39:35 +0545

    feat:register successfull

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const bodyParser = require('body-parser')
-const app = express();
 
-app.use(express.json())
+const app = express();
+const cors = require("cors");
+
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions))
+app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-
-
-
-
-module.exports={
-    app
-}
+module.exports = {
+  app,
+};
