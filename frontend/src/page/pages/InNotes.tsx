abeedc97e4355119821fb7915d76ff64330c6e71
Nishan Bista <nishanbista525@gmail.com>
2024-09-10 00:01:53 +0545

    frontend> some styling

--- frontend/src/page/pages/InNotes.tsx
+++ frontend/src/page/pages/InNotes.tsx
@@ -5,17 +5,21 @@ import {
 	BreadcrumbList,
 	BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
-
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import { fetchEn } from "@/utils/user"
 import { useEffect, useState } from "react"
 import type { NOTE } from "@/types"
+import { Button } from "@chakra-ui/react"
 
 function InNotes() {
 
 	const { title } = useParams()
+	const navigate = useNavigate()
 	const [note, setNote] = useState<NOTE | null>(null)
+	const [editModeTitle, setEditModeTitle] = useState<boolean>(false)
 	console.log(note)
+
+	const breadcStyle = "hover:cursor-pointer hover:text-third"
 
 	useEffect(() => {
 		const fetchNote = async () => {
@@ -37,18 +41,39 @@ function InNotes() {
 		<>
 			<div id="breadc" className="flex justify-center items-center my-2">
 				<Breadcrumb>
-					<BreadcrumbList className="text-text">
+					<BreadcrumbList className="text-text hover:text-white">
 						<BreadcrumbItem>
-							<BreadcrumbLink >Notes</BreadcrumbLink>
+							<BreadcrumbLink className={`${breadcStyle}`} onClick={() => { navigate("/notes") }} >Notes</BreadcrumbLink>
 						</BreadcrumbItem>
 						<BreadcrumbSeparator />
 						<BreadcrumbItem>
-							<BreadcrumbLink href="/components">{title}</BreadcrumbLink>
+							<BreadcrumbLink className={`${breadcStyle}`}>{title}</BreadcrumbLink>
 						</BreadcrumbItem>
 					</BreadcrumbList>
 				</Breadcrumb>
 			</div>
-			<div>InNotes: {title}</div>
+			<div id="noteBody">
+
+				<div id="header">
+
+					<div id="title" className="">
+
+						<label>Title</label>
+						<input value={title} disabled={!editModeTitle} />
+						<Button variant="outline" colorScheme={editModeTitle ? "green" : "cyan"} onClick={
+							() => { setEditModeTitle((prev) => !prev) }}>
+							{editModeTitle ? "Save" : "Edit"}
+						</Button>
+					</div>
+
+
+
+				</div>
+
+
+
+
+			</div>
 		</>
 	)
 }
