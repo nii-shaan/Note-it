e10e0d7adb458a584876beba29fa9c12aa7689c6
Nishan Bista <nishanbista525@gmail.com>
2024-09-12 22:49:40 +0545

    frontend> feat: current note title fetched

--- frontend/src/page/pages/InNotes.tsx
+++ frontend/src/page/pages/InNotes.tsx
@@ -9,7 +9,11 @@ import { useParams, useNavigate } from "react-router-dom"
 import { fetchEn } from "@/utils/user"
 import { useEffect, useState } from "react"
 import type { NOTE } from "@/types"
-import { Button } from "@chakra-ui/react"
+import { useForm, SubmitHandler } from "react-hook-form"
+
+type Inputs = {
+	title: string
+}
 
 function InNotes() {
 
@@ -18,8 +22,27 @@ function InNotes() {
 	const [note, setNote] = useState<NOTE | null>(null)
 	const [editModeTitle, setEditModeTitle] = useState<boolean>(false)
 	console.log(note)
+	const [titleFieldValue, setTitleFieldValue] = useState<string>("")
 
 	const breadcStyle = "hover:cursor-pointer hover:text-third"
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Inputs>({
+		defaultValues:{
+			title
+		}
+	})
+
+	const onSubmit: SubmitHandler<Inputs> = (d) => {
+
+		if (!editModeTitle) {
+			console.log("submitted")
+		}
+
+	}
 
 	useEffect(() => {
 		const fetchNote = async () => {
@@ -28,6 +51,8 @@ function InNotes() {
 			setNote(response.data)
 		}
 		fetchNote()
+
+		setTitleFieldValue(title || "")
 
 
 	}, [])
@@ -38,6 +63,7 @@ function InNotes() {
 		)
 	}
 	return (
+
 		<>
 			<div id="breadc" className="flex justify-center items-center my-2">
 				<Breadcrumb>
@@ -54,25 +80,40 @@ function InNotes() {
 			</div>
 			<div id="noteBody">
 
-				<div id="header">
+				<div id="header" className="mt-5 w-full p-2">
 
-					<div id="title" className="">
+					<form onSubmit={handleSubmit(onSubmit)} className="flex items-center flex-wrap gap-x-3 gap-y-4">
 
-						<label>Title</label>
-						<input value={title} disabled={!editModeTitle} />
-						<Button variant="outline" colorScheme={editModeTitle ? "green" : "cyan"} onClick={
-							() => { setEditModeTitle((prev) => !prev) }}>
-							{editModeTitle ? "Save" : "Edit"}
-						</Button>
-					</div>
+						<label htmlFor="title" className="text-text text-lg font-bold">Title </label >
+						<input id="title" value={titleFieldValue} disabled={!editModeTitle} {...register("title", {
+							required: "Title is required!",
+							pattern: {
+								value: /^[a-zA-Z0-9_-]+$/,
+								message: "'-', '_', Alphabets & Numbers only!"
+							}
+						})} className="bg-transparent border-2 text-text text-center outline-none rounded-lg  py-1 disabled:bg-second enabled:border-green-400 disabled:border-third"
+							onChange={(e) => {
+								setTitleFieldValue(e.target.value)
+							}
 
+							}
+						/>
 
+						{errors.title && <div className="text-red-500 w-full block mt-1 mx-auto">{errors.title.message}</div>}
 
+						<button
+							type="submit"
+							onClick={() => {
+								setEditModeTitle((prev) => errors.title ? true : !prev)
+							}}
+							className={`text-text border-2  py-2 px-4 rounded-lg hover:bg-second 
+								${editModeTitle ? ("border-green-400") : ("border-third")}
+								 border-green-400`}
+						>
+							{editModeTitle ? "Save Title" : "Edit Title"}
+						</button>
+					</form>
 				</div>
-
-
-
-
 			</div>
 		</>
 	)
