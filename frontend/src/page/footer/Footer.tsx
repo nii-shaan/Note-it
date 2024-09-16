b88f7d3d3366c8204901020a1a61937d504546df
Nishan Bista <nishanbista525@gmail.com>
2024-09-16 20:24:28 +0545

    frontend> feat: footer

--- frontend/src/page/footer/Footer.tsx
+++ frontend/src/page/footer/Footer.tsx
@@ -1,7 +1,14 @@
+import { FaRegHeart } from "react-icons/fa";
+
 
 function Footer() {
   return (
-    <div className="h-[200px] w-full bg-second">Footer</div>
+    <div className="h-[100px] w-full bg-second flex items-center justify-center">
+
+
+      <div className="text-text">Made with <FaRegHeart className="inline-block" /> by Nishan</div>
+
+    </div>
   )
 }
 
