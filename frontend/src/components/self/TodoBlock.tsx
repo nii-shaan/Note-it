cde30445b91424797dd4b348aa55312debf4f04b
Nishan Bista <nishanbista525@gmail.com>
2024-09-17 21:09:50 +0545

    frontend> feat: delete remainder

--- frontend/src/components/self/TodoBlock.tsx
+++ frontend/src/components/self/TodoBlock.tsx
@@ -1,9 +1,32 @@
 import type { TODO } from "@/types"
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { MdOutlineInfo, MdDeleteForever } from "react-icons/md";
 
 
 
 function TodoBlock({ todo }: { todo: TODO }) {
+
+  const queryClient = useQueryClient()
+
+  const { mutate } = useMutation({
+    mutationFn: () => {
+      return fetch(`/api/todos/deleteTodo/${todo._id}`, {
+        method: "DELETE",
+        credentials: "include"
+      })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] })
+    }
+  })
+
+
+
+  const handleDeleteTodo = () => {
+    mutate()
+  }
+
+
   return (
     <div className="min-w-[280px] py-1 px-2 border border-green-500 rounded-lg flex items-center justify-between my-2">
       <div>
@@ -13,7 +36,7 @@ function TodoBlock({ todo }: { todo: TODO }) {
         {todo.title}
       </div>
       <div>
-        <MdDeleteForever className="text-red-500 text-2xl hover:text-gray-400 cursor-pointer" />
+        <MdDeleteForever className="text-red-500 text-2xl hover:text-gray-400 cursor-pointer" onClick={handleDeleteTodo} />
       </div>
     </div>
   )
