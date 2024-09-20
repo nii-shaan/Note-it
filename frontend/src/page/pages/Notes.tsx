7ab41f8e3250e778190c345ebf852da2d1b7042a
Nishan Bista <nishanbista525@gmail.com>
2024-09-20 15:29:05 +0545

    frontend> no any notes loader

--- frontend/src/page/pages/Notes.tsx
+++ frontend/src/page/pages/Notes.tsx
@@ -24,7 +24,30 @@ function Notes() {
       {isLoading ? (
         <div>Loading...</div>
       ) : data && data.length === 0 ? (
-        <div>NO NOTES POSTED</div>
+        <div className="w-full flex flex-col items-center mt-32">
+          <div>
+            <div className="loader">
+              <svg viewBox="0 0 80 80">
+                <circle r="32" cy="40" cx="40" id="test"></circle>
+              </svg>
+            </div>
+
+            <div className="loader triangle">
+              <svg viewBox="0 0 86 80">
+                <polygon points="43 8 79 72 7 72"></polygon>
+              </svg>
+            </div>
+
+            <div className="loader">
+              <svg viewBox="0 0 80 80">
+                <rect height="64" width="64" y="8" x="8"></rect>
+              </svg>
+            </div>
+          </div>
+
+          <div className="text-green-500 mt-2">No any Notes</div>
+
+        </div>
       ) : (
         <div className="flex flex-wrap justify-center gap-x-4 gap-y-4 mt-10 pc:justify-start pc:mx-6">
           {data?.map((item: NOTE) => (
