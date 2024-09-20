2d505d3c2e83099eeb8825ecf68b5fdb2404dd2f
Nishan Bista <nishanbista525@gmail.com>
2024-09-20 15:19:02 +0545

    frontend> note block icon

--- frontend/src/components/self/NoteBlock.tsx
+++ frontend/src/components/self/NoteBlock.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom"
+import { CgNotes } from "react-icons/cg";
+
 
 interface NOTEBLOCK {
   title: string
@@ -10,8 +12,11 @@ function NoteBlock({ title }: NOTEBLOCK) {
   return (
     <div className='border border-secondText h-[150px] w-[150px] rounded-lg py-2 px-1 text-text cursor-pointer hover:bg-second hover:scale-95 transition ease-in-out duration-200 inline-block' onClick={() => { navigate(`/notes/${title}`) }}>
 
-      <div id="noteTitle" className="w-full text-center">
+      <div id="noteTitle" className="w-full text-center h-[29%] text-sm overflow-x-scroll">
         {title}
+      </div>
+      <div className="flex justify-center items-center h-[70%]">
+      <CgNotes className="text-[60px] text-third" />
       </div>
 
 
